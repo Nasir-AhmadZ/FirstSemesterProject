89677b8c643453b0275b619fb876df25d1668789
Nasir Ahmad <G00419630@atu.ie>
2022-12-06 20:39:30 +0000

    added slideshow

--- dynamic.js
+++ dynamic.js
@@ -6,3 +6,10 @@ function alerts()
 {
     alert("SORRY AN ERROR HAS OCCURED!");
 }
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
