411a9f57f12395f1c60b47f3b57804a838e9b66e
shine sujith <G00414454@atu.ie>
2022-12-07 19:00:54 +0000

    tested some code

--- dynamic.js
+++ dynamic.js
@@ -28,26 +28,47 @@ function quantity(x)
 
 //change money symbol and enter prices
 document.getElementById("total").innerHTML= "TOTAL:"+"$"+" "+total(quantity(250.00),quantity(76),quantity(54),quantity(67));
-function switch1()
+function switch1(tree)
 {
     const items=['tree', 'jumper', 'shovel', 'PS5'];
-    switch (items)
+    switch (tree)
     {
-        case 'tree':
-            document.getElementById("des1").innerHTML= "$";//Add product desription here
-            document.getElementById("item1").innerHTML= "$"+" "+quantity(250.00);
-            break;
+        case tree:
+           return 250;
+        break;
         case 'jumper':
             document.getElementById("des2").innerHTML= "$";
             document.getElementById("item2").innerHTML= "$"+" "+quantity(76);
-            break;
+        break;
         case 'shovel':
             document.getElementById("des3").innerHTML= "$";
             document.getElementById("item3").innerHTML= "$"+" "+quantity(54);
-            break;
+        break;
         case 'PS5':
             document.getElementById("des4").innerHTML= "";
             document.getElementById("item4").innerHTML= "$"+" "+quantity(67);
-            break;
+        break;
     }
 }
+
+function if1(y)
+{
+    if (y == 1)
+    {
+       return "item1";
+    }
+    else if (y ==2)
+    {
+        document.getElementById("des2").innerHTML= "$";
+        document.getElementById("item2").innerHTML= "$"+" "+quantity(76);
+    }
+}
+
+document.getElementById("des1").innerHTML= "$";//Add product desription here
+document.getElementById("item1").innerHTML= "$"+" "+quantity(250);
+document.getElementById("des2").innerHTML= "$";
+document.getElementById("item2").innerHTML= "$"+" "+quantity(76);
+document.getElementById("des3").innerHTML= "$";
+document.getElementById("item3").innerHTML= "$"+" "+quantity(54);
+document.getElementById("des4").innerHTML= "";
+document.getElementById("item4").innerHTML= "$"+" "+quantity(67);
